d3c612fcc836429d56219d9d158d46482ba4a865
Lucas Wung <59458611+tkoppop@users.noreply.github.com>
2021-02-04 13:15:43 -0800

    Add files via upload

---
+++ CrashtestV1.ps1
@@ -0,0 +1,11 @@
+﻿if (-not(Test-Connection dell-124)){
+    $9
+}
+Test-Connection dell-128
+Test-Connection dell-135
+Test-Connection dell-153
+Test-Connection dell-163
+Test-Connection dell-164
+Test-Connection dell-166
+Test-Connection perform-001
+Test-Connection perform-002
